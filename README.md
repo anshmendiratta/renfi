81469558c8af8e8993d2b7e0089f4125d6305be2
castroploiin <fieryfire1005@gmail.com>
2024-06-14 01:20:22 +0400

    added: styling to options

---
+++ README.md
@@ -0,0 +1,3 @@
+# renamefile_tui
+
+A personal project to avoid manually renaming files. Used in tandem with `[autosort]()`.
--- src/app.rs
+++ src/app.rs
@@ -3,8 +3,9 @@ use std::path::PathBuf;
 use anyhow::Result;
 use crossterm::event::{self, Event, KeyCode};
 use itertools::Itertools;
-use ratatui::style::Stylize;
-use ratatui::widgets::StatefulWidget;
+use ratatui::style::{Style, Stylize};
+use ratatui::widgets::{Block, Borders, HighlightSpacing, StatefulWidget};
+use ratatui::Frame;
 use ratatui::{
     backend::Backend,
     widgets::{ListState, Paragraph, Widget},
@@ -18,18 +19,16 @@ use ratatui::{
 use renamefile_tui::back_logic::rename_file_in_dir;
 
 #[derive(Default)]
-pub struct App<'a> {
+pub struct App {
     state: ListState,
-    names: List<'a>,
-    names_vec: Vec<String>,
+    names: Vec<String>,
 }
 
-impl App<'_> {
+impl App {
     pub fn with_items(items: impl IntoIterator<Item = String> + Clone) -> Self {
         Self {
-            names: List::from_iter(items.clone()),
             state: ListState::default(),
-            names_vec: items.into_iter().collect_vec(),
+            names: items.into_iter().collect(),
         }
     }
 
@@ -40,8 +39,8 @@ impl App<'_> {
             if let Event::Key(key) = event::read()? {
                 match key.code {
                     KeyCode::Esc => break,
-                    KeyCode::Up => self.previous_selection(),
-                    KeyCode::Down => self.next_selection(),
+                    KeyCode::Up => self.previous(),
+                    KeyCode::Down => self.next(),
                     KeyCode::Enter => {
                         let file_to_rename: &str = &std::env::args().collect_vec()[1];
                         self.rename_file(file_to_rename)?
@@ -60,34 +59,48 @@ impl App<'_> {
     }
 
     fn render_title(&self, area: Rect, buf: &mut Buffer) {
-        Paragraph::new("Rename file")
+        Paragraph::default()
             .centered()
             .bold()
+            .block(Block::default().title("Rename file").borders(Borders::ALL))
             .render(area, buf);
     }
 
-    // TODO: Make text area that auto selects upon <enter>
     fn render_list(&self, area: Rect, buf: &mut Buffer) {
-        let layout =
-            Layout::new(Direction::Vertical, Constraint::from_fills([1])).flex(Flex::Center);
-        let [area] = layout.areas(area);
+        let layout_vertical = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(Constraint::from_fills([1, 1, 1]))
+            .flex(Flex::Center)
+            .split(Rect::new(1, 1, 1));
+        let layout_horizontal = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(Constraint::from_fills([1, 1, 1]))
+            .flex(Flex::Center)
+            .split(layout_vertical[1]);
 
-        StatefulWidget::render(self.names.clone(), area, buf, &mut self.state.clone());
+        let list = List::new(self.names.clone())
+            .highlight_style(Style::new().green())
+            .highlight_symbol("> ")
+            .highlight_spacing(HighlightSpacing::WhenSelected)
+            .slow_blink()
+            .not_bold();
+
+        StatefulWidget::render(list, area_horiz, buf, &mut self.state.clone());
     }
 
-    fn next_selection(&mut self) {
+    fn next(&mut self) {
         let new_selection = self.state.selected().unwrap_or(1).saturating_add(1);
         self.state.select(Some(new_selection));
     }
 
-    fn previous_selection(&mut self) {
+    fn previous(&mut self) {
         let new_selection = self.state.selected().unwrap_or(1).saturating_sub(1);
         self.state.select(Some(new_selection));
     }
 
     fn rename_file(&self, file_to_rename: &str) -> Result<()> {
         let chosen_option_idx: usize = self.state.selected().unwrap_or(0);
-        let chosen_file_name: &str = &self.get_items_as_vec()[chosen_option_idx];
+        let chosen_file_name: &str = &self.names[chosen_option_idx];
         let directory_called_in: &str = &std::env::args().collect_vec()[0];
 
         rename_file_in_dir(
@@ -105,22 +118,12 @@ impl App<'_> {
     }
 
     #[allow(dead_code)]
-    fn get_items(&self) -> List {
+    fn get_items(&self) -> Vec<String> {
         self.names.clone()
-    }
-
-    #[allow(dead_code)]
-    fn get_items_as_vec(&self) -> Vec<String> {
-        let mut items_vec = Vec::new();
-        for item in self.names_vec.clone() {
-            items_vec.push(item);
-        }
-
-        items_vec
     }
 }
 
-impl Widget for &mut App<'_> {
+impl Widget for &mut App {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer) {
         self.render_title(area, buf);
         self.render_list(area, buf);
@@ -134,14 +137,14 @@ mod tests {
     #[test]
     fn check_add() {
         let mut app = App::default();
-        app.next_selection();
+        app.next();
         assert_eq!(app.get_state().selected(), Some(2));
     }
 
     #[test]
     fn check_subtract() {
         let mut app = App::default();
-        app.previous_selection();
+        app.previous();
         assert_eq!(app.get_state().selected(), Some(0));
     }
 }
